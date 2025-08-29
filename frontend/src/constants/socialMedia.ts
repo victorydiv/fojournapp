f1157940b8c8c73df48d5f8ac95f460a023b4cc1
Sean Estes <sean@estes-family.org>
2025-08-29 14:00:16 -0500

    Update Facebook link to correct profile URL
    - Change Facebook link from generic /fojourn to actual profile ID
    - Link now points to https://www.facebook.com/profile.php?id=61579768918228

--- frontend/src/constants/socialMedia.ts
+++ frontend/src/constants/socialMedia.ts
@@ -1,6 +1,6 @@
 // Social Media Links Configuration
 export const SOCIAL_MEDIA_LINKS = {
-  facebook: 'https://www.facebook.com/fojourn',
+  facebook: 'https://www.facebook.com/profile.php?id=61579768918228',
   linkedin: 'https://www.linkedin.com/company/fojourn',
   // Add more platforms as needed
   // twitter: 'https://twitter.com/fojourn',
